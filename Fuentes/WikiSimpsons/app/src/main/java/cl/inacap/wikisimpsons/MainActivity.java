f540d24bd700e4539fcb5dd3d191e1f92ac76b69
unknown <joshua.gallardo.jvga@gmail.com>
2020-11-29 14:49:55 -0400

    Creación de Toolbar y elementos basicos
    Colocar icono simpson
    Generar toolbar, spinner, button y listview

---
+++ Fuentes/WikiSimpsons/app/src/main/java/cl/inacap/wikisimpsons/MainActivity.java
@@ -0,0 +1,16 @@
+package cl.inacap.wikisimpsons;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+    }
+}
